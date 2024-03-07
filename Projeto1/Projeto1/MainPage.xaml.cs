f04c670e84917ad174f23df6254d43e5823cf800
PedroLudovic144 <pedroludovic5@gmail.com>
2024-03-07 12:04:08 -0300

    Chore:Add files and folders

---
+++ Projeto1/Projeto1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Projeto1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name = string.Empty;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnGreetCliked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+            GreetLabel.Text = $"Hello, {name}!";
+        }
+    }
+
+}
